266384f5a69ea134d3531f517d611d83a1abd2bb
SafaaKiwan <safaakiwan@gmail.com>
2021-04-14 17:20:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal<= 0){
+        document.writeln("Enter a positive number");
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result = document.getElementById("result");
+    document.writeln("If you deposit ",principal );
+    document.writeln("at an interest rate of", rate);
+    document.writeln("You will receive an amount of",175000);
+    document.writeln("in the year", years.value);
+
 }
-        
+          function updateRate() 
+      {
+          var rateval = document.getElementById("rate").value;
+          document.getElementById("rate_val").innerText=rateval;
+      }
